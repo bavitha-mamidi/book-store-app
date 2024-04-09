b71024572877cdb2cc765bfe7209ca4cfd44a823
bavitha <bavithareddymamidi@gmail.com>
2024-04-09 06:28:52 +0530

    Refactoring the name

--- Bookstore_App_Vikas_Tripathi/src/com/mycompany/bookstore/service/BookService.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.bookstore.service;
-
-import com.mycompany.bookstore.dto.BookDTO;
-
-public interface BookService {
-
-    public Long add(BookDTO bookDTO);
-    public BookDTO getBook(Long bookId);
-}
